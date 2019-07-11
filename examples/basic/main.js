d46bbf8874a1fdc9bc5da14cefe4e4481937a270
Domenic Matesic <domenic.matesic@gmail.com>
2019-07-10 20:27:46 -0400

    Add basic example

---
+++ examples/basic/main.js
@@ -0,0 +1,46 @@
+const { join } = require('path');
+const { app, dialog, BrowserWindow } = require('electron');
+const { handleAuth } = require('./auth');
+
+let win;
+
+function createWindow() {
+  handleAuth()
+    .then((userProfile) => {
+      win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        center: true
+      });
+
+      console.log(`login email: "${userProfile.email}"`);
+
+      win.loadURL(`file://${join(__dirname, 'index.html')}`);
+      win.on('closed', () => {
+        win = null;
+      });
+    })
+    .catch((err) => handlerError('authentication error', err));
+}
+
+function handlerError(message, err) {
+  console.error(err);
+
+  dialog.showMessageBox({ type: 'error', message }, () => {
+    app.quit();
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (win === null) createWindow();
+});
+
+process.on('uncaughtException', (err) => {
+  handlerError('uncaught exception', err);
+});
